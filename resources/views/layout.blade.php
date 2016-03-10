3edb430c26492c4506e9ca9d5f895c0037637b26
anewage <amirh2008@gmail.com>
2016-03-10 20:07:36 +0330

    initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,214 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+    <title>دومین دوره مسابقات برنامه‌نویسی دانش‌آموزی امیرکبیر</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
+<body class="landing">
+<div id="page-wrapper">
+
+    <!-- Header -->
+    <header id="header">
+        <h1 id="logo"><a href="{{route('home')}}">مسابقات برنامه‌نویسی دانش‌آموزی</a></h1>
+        <nav id="nav">
+            <ul>
+                <li><a href="{{url("about")}}">درباره مسابقات</a></li>
+                <li>
+                    <a href="{{url("past_contests")}}">دوره‌های گذشته</a>
+                    {{--<ul>--}}
+                    {{--<li><a href="left-sidebar.html">Left Sidebar</a></li>--}}
+                    {{--<li><a href="right-sidebar.html">Right Sidebar</a></li>--}}
+                    {{--<li><a href="no-sidebar.html">No Sidebar</a></li>--}}
+                    {{--<li>--}}
+                    {{--<a href="#">Submenu</a>--}}
+                    {{--<ul>--}}
+                    {{--<li><a href="#">Option 1</a></li>--}}
+                    {{--<ul>--}}
+                    {{--<li><a href="#">Option 1</a></li>--}}
+                    {{--</ul>--}}
+                    {{--<li><a href="#">Option 2</a></li>--}}
+                    {{--<li><a href="#">Option 3</a></li>--}}
+                    {{--<li><a href="#">Option 4</a></li>--}}
+                    {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--</ul>--}}
+                </li>
+                <li><a href="{{url("contact")}}">ارتباط با ما</a></li>
+                <li><a href="{{url("register")}}" class="button special">ثبت‌نام</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Banner -->
+    <section id="banner">
+        <div class="content">
+            <header>
+                <h2>دومین دوره مسابقات ملی برنامه‌نویسی دانش‌آموزی امیرکبیر</h2>
+                <p>۲۶ فروردین ماه<br />
+                    دانشگاه صنعتی امیرکبیر</p>
+            </header>
+            <span class="image"><img src="../images/ACMLOGO.jpg" alt="" /></span>
+        </div>
+        <a href="#one" class="goto-next"></a>
+    </section>
+
+    <!-- One -->
+    <section id="one" class="spotlight style1 bottom">
+        <span class="image fit main"><img src="images/code.jpg" alt="" /></span>
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="4u 12u$(medium)">
+                        <header>
+                            <h2>یک دو سه</h2>
+                            <p>زنگ مدرسه</p>
+                        </header>
+                    </div>
+                    <div class="4u 12u$(medium)">
+                        <p>محل نوشتن توضیحات
+                        هرچی دل تنگت میخواد بنویس اینجا، مثلاً اینکه دوره‌های گذشته چیکار کردیم.</p>
+                    </div>
+                    <div class="4u$ 12u$(medium)">
+                        <p>بازم توضیحات.
+                        فلان بیسار</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="#two" class="goto-next scrolly"></a>
+    </section>
+
+    <!-- Two -->
+    <section id="two" class="spotlight style2 right">
+        <span class="image fit main"><img src="images/tsb.jpg" alt="" /></span>
+        <div class="content">
+            <header>
+                <h2>فکر کنید، حل کنید، بادکنک بگیرید!</h2>
+                <p>یکم بیشتر درباره مسابقه صحبت می‌کنیم.</p>
+            </header>
+            <p>حالا میگیم که اگه میخوای بیشتر بخونی روی دکمه زیر بزن:</p>
+            <ul class="actions">
+                <li><a href="#" class="button">اطلاعات بیشتر</a></li>
+            </ul>
+        </div>
+        <a href="#three" class="goto-next scrolly"></a>
+    </section>
+
+    <!-- Three -->
+    <section id="three" class="spotlight style3 left">
+        <span class="image fit main bottom"><img src="images/code.jpg" alt="" /></span>
+        <div class="content">
+            <header>
+                <h2>حالا میگیم دانشگاه امیرکبیر کجاس</h2>
+                <p>حتی این‌که انجمنمون کیه و چیکارست و اینا.</p>
+            </header>
+            <p>باید پر بشه دیگه کلاْ</p>
+            <ul class="actions">
+                <li><a href="#" class="button">اطلاعات بیشتر</a></li>
+            </ul>
+        </div>
+        <a href="#four" class="goto-next scrolly"></a>
+    </section>
+
+    <!-- Four -->
+    <section id="four" class="wrapper style1 special fade-up">
+        <div class="container">
+            <header class="major">
+                <h2>حامیان دومین دوره مسابقات</h2>
+                <p>لیست موسسات و برندهایی که از دومین دوره مسابقات ملی برنامه‌نویسی دانش‌آموزی امیرکبیر حمایت می‌کنند</p>
+            </header>
+            <div class="box alt">
+                <div class="row uniform">
+                    <section class="4u 6u(medium) 12u$(xsmall)">
+                        <span class="icon alt major fa-area-chart"></span>
+                        <h3>Ipsum sed commodo</h3>
+                        <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                    </section>
+                    <section class="4u 6u$(medium) 12u$(xsmall)">
+                        <span class="icon alt major fa-comment"></span>
+                        <h3>Eleifend lorem ornare</h3>
+                        <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                    </section>
+                    <section class="4u$ 6u(medium) 12u$(xsmall)">
+                        <span class="icon alt major fa-flask"></span>
+                        <h3>Cubilia cep lobortis</h3>
+                        <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                    </section>
+                    <section class="4u 6u$(medium) 12u$(xsmall)">
+                        <span class="icon alt major fa-paper-plane"></span>
+                        <h3>Non semper interdum</h3>
+                        <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                    </section>
+                    <section class="4u 6u(medium) 12u$(xsmall)">
+                        <span class="icon alt major fa-file"></span>
+                        <h3>Odio laoreet accumsan</h3>
+                        <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                    </section>
+                    <section class="4u$ 6u$(medium) 12u$(xsmall)">
+                        <span class="icon alt major fa-lock"></span>
+                        <h3>Massa arcu accumsan</h3>
+                        <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Five -->
+    <section id="five" class="wrapper style2 special fade">
+        <div class="container">
+            <header class="major">
+                <h2>محل برگزاری</h2>
+                <p>دانشکده‌ی مهندسی کامپیوتر و فناوری اطلاعات دانشگاه صنعتی امیرکبیر</p>
+                <p>تهران، چهارراه ولی عصر، روبه‌روی خیابان بزرگمهر، دانشگاه صنعتی امیرکبیر</p>
+                <p>جهت کسب اطلاعات بیش‌تر به دفتر انجمن علمی دانشکده مراجعه فرمایید.</p>
+            </header>
+            <div id="map"></div>
+            <script>
+                function initMap() {
+                    var mapDiv = document.getElementById('map');
+                    var map = new google.maps.Map(mapDiv, {
+                        center: {lat: 35.70392, lng: 51.40972},
+                        zoom: 16
+                    });
+                }
+            </script>
+            <script src="https://maps.googleapis.com/maps/api/js?callback=initMap"
+                    async defer></script>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <ul class="icons">
+            <li><a href="https://twitter.com/ceit_ssc" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="https://www.facebook.com/ceit.ssc.official" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="https://www.instagram.com/ceit_ssc/" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+            <li><a href="mailto:ceit.ssc@aut.ac.ir" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+        </ul>
+        <ul class="copyright">
+            <li>انجمن علمی دانشکده مهندسی کامپیوتر و فناوری اطلاعات دانشگاه صنعتی امیرکبیر&copy;</li>
+        </ul>
+    </footer>
+
+</div>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/jquery.dropotron.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
