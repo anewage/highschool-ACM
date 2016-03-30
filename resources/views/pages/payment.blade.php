134fcb3584689e87f89fb92a6dd6738efc369696
anewage <amirh2008@gmail.com>
2016-03-30 17:54:07 +0430

    bank gateway changed

--- resources/views/pages/payment.blade.php
+++ resources/views/pages/payment.blade.php
@@ -5,7 +5,7 @@
         <h2>چند قدم تا تکمیل ثبت نام مانده...</h2>
         <p>برای تکمیل ثبت‌نام خود،‌لطفاْ‌هزینه شرکت در مسابقه را از طریق درگاه پرداخت زیر پرداخت نموده و شماره فاکتور را در زیر وارد کنید:</p>
         {{Form::open(['route' => 'payment', 'target' => '_blank' ])}}
-        {{Form::hidden('api', '34763-hdemf-47571-88452-xuv6f8fc4o4vi7p1vg46767458')}}
+        {{Form::hidden('api', '23842-bpuyg-26230-78367-s8f43ae28gk2vi708883265480')}}
         {{Form::hidden('price','300000')}}
         {{Form::hidden('callback', 'https://highschool.icpc.aut.ac.ir/payment_done?id='.$record['id'] )}}
         {{Form::hidden('description', 'AUT High School Programming Contest payment' )}}
@@ -20,7 +20,7 @@
         @foreach($record as $key => $value)
             {!! Form::hidden($key, $value) !!}
         @endforeach
-        {{Form::hidden('api', '34763-hdemf-47571-88452-xuv6f8fc4o4vi7p1vg46767458')}}
+        {{Form::hidden('api', '23842-bpuyg-26230-78367-s8f43ae28gk2vi708883265480')}}
         {{Form::hidden('price','300000')}}
         <span>شماره فاکتور:</span>{{ Form::text('payment_id', null, ['class' => '6u 12u$(xsmall)', 'placeholder' => 'شماره فاکتور پرداخت']) }}
         {{Form::submit('تکمیل ثبت‌نام',['class' => 'special'])}}
